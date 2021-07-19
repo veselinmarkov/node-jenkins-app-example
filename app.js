072dda9fa032e1e4f6fb0fa3f395e6e7f88b1166
Veselin Markov <48570558+veselinmarkov@users.noreply.github.com>
2021-07-19 20:35:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
-
-var server = app.listen(3000, () => {
+var port   = process.env.NODE_PORT || 3000;
+var server = app.listen(port, () => {
     console.log("Listening on port " + server.address().port + "...");
 });
 
